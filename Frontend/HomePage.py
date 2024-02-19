4645d301754d6a5733d22f1450bf824b276061fb
tanish solanki <tanishs4313@gmail.com>
2024-02-19 20:51:56 +0530

    Added Home Page

--- Frontend/HomePage.py
+++ Frontend/HomePage.py
@@ -5,5 +5,39 @@ class HomePage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.master = master
-        self.label_username = tk.Label(self, text="Username:")
-        self.label_username.pack()
+
+        self.top_bar_frame = tk.Frame(self, bg="black", height=20)
+        self.name_label = tk.Label(self.top_bar_frame, text="name")
+        self.name_label.pack()
+
+        self.add_money_button = tk.Button(
+            self, text="Add Cash", command=lambda: self.add_money()
+        )
+        self.withdraw_money_button = tk.Button(
+            self, text="Withdraw Cash", command=lambda: self.withdraw_money()
+        )
+        self.transfer_money_button = tk.Button(
+            self, text="Tranfer", command=lambda: self.transfer_money()
+        )
+        self.show_transation_button = tk.Button(
+            self, text="Show all transtions", command=lambda: self.show_transation()
+        )
+        self.top_bar_frame.grid_configure(columnspan=2)
+        self.top_bar_frame.grid(column=0, row=0)
+        self.add_money_button.grid(column=0, row=1, padx=10, pady=10)
+        self.withdraw_money_button.grid(column=1, row=1, padx=10, pady=10)
+        self.transfer_money_button.grid(column=0, row=2, padx=10, pady=10)
+        self.show_transation_button.grid(column=1, row=2, padx=10, pady=10)
+
+    # TODO: add db
+    def add_money(self):
+        print("deposit")
+
+    def withdraw_money(self):
+        print("Withdraw")
+
+    def transfer_money(self):
+        print("tranfer")
+
+    def show_transation(self):
+        print("show transation")
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from Frontend import App
 
 if __name__ == "__main__":
     app = App.MainApplication()
-    app.geometry("800x800")
+    app.geometry("400x400")
     app.mainloop()
